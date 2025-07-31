0699de7f259f0cd762b20be77144f6b989916b83
Milton Rosa <rosamilton99@gmail.com>
2025-07-30 21:41:40 -0400

    Reading JSON file
    Created calculate_pay function that can now read and print the data from the JSON file

--- Submission/main.py
+++ Submission/main.py
@@ -1,5 +1,17 @@
-def main():
-    print("Hello, World!")
+import json
+
+
+##Calculate Pay Function
+# This function reads employee punch data from a JSON file and calculates their pay.
+def calculate_pay():
+    with open("InputData.json", "r") as file:
+        employee_data = json.load(file)
+        job_meta = employee_data["jobMeta"]
+        employee_punches = employee_data["employeeData"]
+
+        print(json.dumps(employee_punches, indent=2))
+        print(print(json.dumps(job_meta, indent=2)))
+
 
 if __name__ == "__main__":
-    main()
+    calculate_pay()
