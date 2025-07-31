b73d2970e9be88991e5c25b9cd809eaf9ebfcfb7
Milton Rosa <rosamilton99@gmail.com>
2025-07-30 21:51:12 -0400

    Seperated Concerns
    Decided to seperate concerns. Created read_JSON that is only going to read the FILE and pass that information into calculate_pay. Function still needs to be worked on. Now it just prints out the data

--- Submission/main.py
+++ Submission/main.py
@@ -1,17 +1,19 @@
 import json
 
 
-##Calculate Pay Function
-# This function reads employee punch data from a JSON file and calculates their pay.
-def calculate_pay():
+# Reads JSON file and parses data
+def read_JSON():
     with open("InputData.json", "r") as file:
         employee_data = json.load(file)
         job_meta = employee_data["jobMeta"]
         employee_punches = employee_data["employeeData"]
+        calculate_pay(job_meta, employee_punches)
 
-        print(json.dumps(employee_punches, indent=2))
-        print(print(json.dumps(job_meta, indent=2)))
+
+def calculate_pay(job_meta, employee_punches):
+    print(json.dumps(job_meta, indent=2))
+    print(json.dumps(employee_punches, indent=2))
 
 
 if __name__ == "__main__":
-    calculate_pay()
+    read_JSON()
