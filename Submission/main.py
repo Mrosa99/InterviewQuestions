18df278b51a8faff04ce78ac3d54d69b7cc7ea4c
Milton Rosa <rosamilton99@gmail.com>
2025-07-31 01:19:07 -0400

    Updates to functions
    JSON reader function now reads all .json files within the folder. To remove the hardcoded file path. Updated function comments to better explain what the functions do

--- Submission/main.py
+++ Submission/main.py
@@ -1,27 +1,38 @@
 import json
 from datetime import datetime
+from pathlib import Path
 
 
-def read_JSON():
+def read_JSON(folder_path):
     """
-    Reads and parses the JSON file named 'InputData.json'.
-    Extracts job metadata and employee punch data.
-    Calls calculate_pay to process the data.
+    Reads all JSON files in the given folder and processes employee data.
+    - Iterates through each .json file.
+    - Parses the file content.
+    - Validates that required keys ('jobMeta' and 'employeeData') are present.
+    - Passes valid data to the pay calculation function.
     """
-    with open("InputData.json", "r") as file:
-        employee_data = json.load(file)
-        job_meta = employee_data["jobMeta"]
-        employee_punches = employee_data["employeeData"]
-        calculate_pay(job_meta, employee_punches)
+    json_files = Path(folder_path).glob("*.json")
+
+    for file_path in json_files:
+        with open(file_path, "r") as file:
+            employee_data = json.load(file)
+
+            job_meta = employee_data.get("jobMeta")
+            employee_punches = employee_data.get("employeeData")
+            if not job_meta:
+                print(f'Missing "jobMeta" in: {file_path} => Skipping file')
+                continue
+            if not employee_punches:
+                print(f'Missing "employeeData" in: {file_path}=> Skipping file')
+                continue
+            calculate_pay(job_meta, employee_punches)
 
 
 def calculate_pay(job_meta, employee_punches):
     """
     Processes employee punches and job metadata to calculate pay.
-    Outputs the results in a dictionary for each employee.
-    """
     output = {}
-
+    """
     print(json.dumps(job_meta, indent=2))
     for emp in employee_punches:
         employee_name = emp["employee"]
@@ -30,10 +41,10 @@ def calculate_pay(job_meta, employee_punches):
         for tp in emp["timePunch"]:
             s = datetime.strptime(tp["start"], "%Y-%m-%d %H:%M:%S")
             e = datetime.strptime(tp["end"], "%Y-%m-%d %H:%M:%S")
-            shift_duration = round((e - s).total_seconds() / 3600.0, 4)
-            print(shift_duration)
+            hrs_worked = round((e - s).total_seconds() / 3600.0, 4)
+            print(hrs_worked)
         print()
 
 
 if __name__ == "__main__":
-    read_JSON()
+    read_JSON(".")
